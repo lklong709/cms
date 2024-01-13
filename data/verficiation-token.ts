d9457f7b8f19769f3e21d90af203553d22b00354
lklong709 <lklong2203@gmail.com>
2024-01-13 19:41:26 +0700

    Initial Setup

---
+++ data/verficiation-token.ts
@@ -0,0 +1,29 @@
+import { db } from "@/lib/db";
+
+export const getVerificiationTokenByToken = async (token: string) => {
+  try {
+    const verificationToken = await db.verificationToken.findUnique({
+      where: {
+        token,
+      },
+    });
+
+    return verificationToken;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const getVerificiationTokenByEmail = async (email: string) => {
+  try {
+    const verificationToken = await db.verificationToken.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    return verificationToken;
+  } catch (error) {
+    return null;
+  }
+};
---
+++ lib/token.ts
@@ -0,0 +1,48 @@
+import { v4 as uuidv4 } from "uuid";
+import { getVerificiationTokenByEmail } from "@/data/verficiation-token";
+import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
+import { db } from "@/lib/db";
+
+export const generateVerificationToken = async (email: string) => {
+  const token = uuidv4();
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getVerificiationTokenByEmail(email);
+
+  if (existingToken) {
+    await db.verificationToken.delete({ where: { id: existingToken.id } });
+  }
+
+  const verificationToken = await db.verificationToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return verificationToken;
+};
+
+export const generatePasswordResetToken = async (email: string) => {
+  const token = uuidv4();
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getPasswordResetTokenByEmail(email);
+
+  if (existingToken) {
+    await db.passwordResetToken.delete({
+      where: { id: existingToken.id },
+    });
+  }
+
+  const passwordResetToken = await db.passwordResetToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return passwordResetToken;
+};
